b41d079af72624234b1ddf431b4e2eaf7ed5a63a
achmdrzl <achmd.rzl13@gmail.com>
2023-05-16 12:09:37 +0700

    templating

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,202 @@
+    <!-- Main Menu -->
+    <div data-simplebar class="nicescroll-bar">
+        <div class="menu-content-wrap">
+            <div class="menu-group">
+                <ul class="navbar-nav flex-column">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.html">
+                            <span class="nav-link-text">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">
+                            <span class="nav-link-text">Data Barang</span>
+                        </a>
+                    </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="email.html">
+                            <span class="nav-link-text">Penjualan</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="email.html">
+                            <span class="nav-link-text">Pembelian</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="email.html">
+                            <span class="nav-link-text">Cetak Barcode</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#masterdata">
+                            <span class="nav-link-text">Master Data</span>
+                        </a>
+                        <ul id="masterdata" class="nav flex-column collapse nav-children">
+                            <li class="nav-item">
+                                <ul class="nav flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats.html"><span class="nav-link-text">Customer</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-group.html"><span class="nav-link-text">Supplier</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Model</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Satuan</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Pabrik</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Kadar</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Barang</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Harga Jual</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Merk</span></a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#transaksi">
+                            <span class="nav-link-text">Transaksi</span>
+                        </a>
+                        <ul id="transaksi" class="nav flex-column collapse   nav-children">
+                            <li class="nav-item">
+                                <ul class="nav flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#dash_wizard">
+                                            <span class="nav-link-text">Penjualan</span>
+                                        </a>
+                                        <ul id="dash_wizard" class="nav flex-column collapse   nav-children">
+                                            <li class="nav-item">
+                                                <ul class="nav flex-column">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="chatpopup.html"><span class="nav-link-text">Input Penjualan</span></a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="chatbot.html"><span class="nav-link-text">Daftar Penjualan</span></a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats.html"><span class="nav-link-text">Return Penjualan</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-group.html"><span class="nav-link-text">Supplier</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Pembelian</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Lebur/Cuci/Reparasi</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Penerimaan Barang</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Hutang</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Kode Kas</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Pengeluaran</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-contact.html"><span class="nav-link-text">Pendapatan Lain</span></a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#laporan">
+                            <span class="nav-link-text">Laporan</span>
+                        </a>
+                        <ul id="laporan" class="nav flex-column collapse   nav-children">
+                            <li class="nav-item">
+                                <ul class="nav flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats.html"><span class="nav-link-text">Stock</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats.html"><span class="nav-link-text">History Perhiasan</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#dash_wizard">
+                                            <span class="nav-link-text">Penjualan</span>
+                                        </a>
+                                        <ul id="dash_wizard" class="nav flex-column collapse   nav-children">
+                                            <li class="nav-item">
+                                                <ul class="nav flex-column">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="chatpopup.html"><span class="nav-link-text">Rekap Penjualan</span></a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="chatbot.html"><span class="nav-link-text">Detail Penjualan</span></a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#dash_wizard">
+                                            <span class="nav-link-text">Return Penjualan</span>
+                                        </a>
+                                        <ul id="dash_wizard" class="nav flex-column collapse   nav-children">
+                                            <li class="nav-item">
+                                                <ul class="nav flex-column">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="chatpopup.html"><span class="nav-link-text">Rekap Return Penjualan</span></a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="chatbot.html"><span class="nav-link-text">Detail Return Penjualan</span></a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#dash_wizard">
+                                            <span class="nav-link-text">Pembelian</span>
+                                        </a>
+                                        <ul id="dash_wizard" class="nav flex-column collapse   nav-children">
+                                            <li class="nav-item">
+                                                <ul class="nav flex-column">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="chatpopup.html"><span class="nav-link-text">Direct Message</span></a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="chatbot.html"><span class="nav-link-text">Chatbot Chat</span></a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats.html"><span class="nav-link-text">Lebur/Cuci/Reparasi</span></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="chats-group.html"><span class="nav-link-text">Cash on Flow</span></a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>	
+            </div>
+        </div>
+    </div>
+    <!-- /Main Menu -->
